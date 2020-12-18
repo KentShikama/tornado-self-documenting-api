0f9ec44c25833504903c28ea069275f4ce472049
Kent Alan Shikama <kent@bright.md>
2020-12-17 23:47:30 -0800

    Initial commit

---
+++ tornado_api_validation_demo/openapi_util.py
@@ -0,0 +1,23 @@
+import json
+
+from apispec import APISpec
+from apispec.exceptions import APISpecError
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.tornado import TornadoPlugin
+
+
+def generate_openapi_json(handlers, file_location):
+    spec = APISpec(
+        title="Tornado self-documenting API validation demo",
+        version="1.0.0",
+        openapi_version="3.0.2",
+        info=dict(description="A minimal API"),
+        plugins=[TornadoPlugin(), MarshmallowPlugin()],
+    )
+    for handler in handlers:
+        try:
+            spec.path(urlspec=handler)
+        except APISpecError:
+            pass
+    with open(file_location, "w", encoding="utf-8") as file:
+        json.dump(spec.to_dict(), file, indent=2)
