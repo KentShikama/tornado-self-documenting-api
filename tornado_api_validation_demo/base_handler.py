e855f800cd212f8150383cc4e8e79f5164599ae1
Kent Alan Shikama <kent@bright.md>
2020-12-17 23:50:47 -0800

    Extract decorators and base handler to module

---
+++ tornado_api_validation_demo/base_handler.py
@@ -0,0 +1,71 @@
+import functools
+import json
+from typing import Type
+
+from tornado.web import RequestHandler
+from marshmallow import Schema, ValidationError
+
+
+class BaseHandler(RequestHandler):
+    """Application base handler"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.validated_body = {}
+
+    def write_json(self, status, data):
+        self.set_header("Content-Type", "application/json")
+        self.set_status(status)
+        self.write(json.dumps(data, default=str))
+        self.finish()
+
+
+def params(schema: Type[Schema]):
+    def decorator(func):
+        @functools.wraps(func)
+        def add_validation_and_doc(self, *args, **kwargs):
+            try:
+                self.validated_body = schema().loads(self.request.body)
+                return func(self, *args, **kwargs)
+            except ValidationError as e:
+                self.write_json(400, e.messages)
+            except Exception as e:
+                self.write_json(400, str(e))
+        doc_string = f"""
+        {func.__doc__}
+        requestBody:
+            description: {schema.__doc__}
+            required: True
+            content:
+                application/json:
+                    schema:
+                        {schema.__name__}
+        """
+        add_validation_and_doc.__doc__ = doc_string
+        return add_validation_and_doc
+    return decorator
+
+
+def success(schema: Type[Schema]):
+    def decorator(func):
+        @functools.wraps(func)
+        async def add_response_and_doc(self, *args, **kwargs):
+            result = await func(self, *args, **kwargs)
+            errors = schema().validate(result)
+            if errors:
+                self.write_json(400, errors)
+            else:
+                self.write_json(200, result)
+        doc_string = f"""
+        {func.__doc__}
+        responses:
+            200:
+                description: {schema.__doc__}
+                content:
+                    application/json:
+                        schema:
+                            {schema.__name__}
+        """
+        add_response_and_doc.__doc__ = doc_string
+        return add_response_and_doc
+    return decorator
--- tornado_api_validation_demo/main.py
+++ tornado_api_validation_demo/main.py
@@ -1,12 +1,9 @@
-import functools
-import json
-from typing import Type
-
 import swagger_ui
 import tornado.ioloop
-from tornado.web import HTTPError, RequestHandler
-from marshmallow import Schema, fields, ValidationError
+from tornado.web import HTTPError
+from marshmallow import Schema, fields
 
+from tornado_api_validation_demo.base_handler import BaseHandler, params, success
 from tornado_api_validation_demo.openapi_util import generate_openapi_json
 
 
@@ -16,71 +13,6 @@ class FooParameter(Schema):
 
 class FooSchema(Schema):
     output = fields.Int()
-
-
-class BaseHandler(RequestHandler):
-    """Application base handler"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.validated_body = {}
-
-    def write_json(self, status, data):
-        self.set_header("Content-Type", "application/json")
-        self.set_status(status)
-        self.write(json.dumps(data, default=str))
-        self.finish()
-
-
-def params(schema: Type[Schema]):
-    def decorator(func):
-        @functools.wraps(func)
-        def add_validation_and_doc(self, *args, **kwargs):
-            try:
-                self.validated_body = schema().loads(self.request.body)
-                return func(self, *args, **kwargs)
-            except ValidationError as e:
-                self.write_json(400, e.messages)
-            except Exception as e:
-                self.write_json(400, str(e))
-        doc_string = f"""
-        {func.__doc__}
-        requestBody:
-            description: {schema.__doc__}
-            required: True
-            content:
-                application/json:
-                    schema:
-                        {schema.__name__}
-        """
-        add_validation_and_doc.__doc__ = doc_string
-        return add_validation_and_doc
-    return decorator
-
-
-def success(schema: Type[Schema]):
-    def decorator(func):
-        @functools.wraps(func)
-        async def add_response_and_doc(self, *args, **kwargs):
-            result = await func(self, *args, **kwargs)
-            errors = schema().validate(result)
-            if errors:
-                self.write_json(400, errors)
-            else:
-                self.write_json(200, result)
-        doc_string = f"""
-        {func.__doc__}
-        responses:
-            200:
-                description: {schema.__doc__}
-                content:
-                    application/json:
-                        schema:
-                            {schema.__name__}
-        """
-        add_response_and_doc.__doc__ = doc_string
-        return add_response_and_doc
-    return decorator
 
 
 class MainHandler(BaseHandler):
